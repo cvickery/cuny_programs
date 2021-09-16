3d456c15de4dfb23fcc5a1f04aa0dd5203c997e8
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2021-09-16 10:34:54 -0400

    Add scribe_to_html.py
    Refactoring the to_html() function into its own module.

---
+++ dgw_info/scribe_to_html.py
@@ -0,0 +1,27 @@
+#! /usr/local/bin/python3
+
+from dgw_filter import dgw_filter
+
+
+# to_html()
+# -------------------------------------------------------------------------------------------------
+def to_html(requirement_text):
+  """ Generate a HTML details element for the code of a Scribe Block.
+  """
+  # catalog_type, first_year, last_year, catalog_years_text = catalog_years(row.period_start,
+  #                                                                         row.period_stop)
+  # institution_name = institution_names[row.institution]
+  filtered_text = dgw_filter(row.requirement_text)
+  html = f"""
+<details>
+  <summary><strong>Degree Works Code</strong> (<em>Scribe Block</em>)</summary>
+  <hr>
+  <pre>{filtered_text.replace('<', '&lt;')}</pre>
+</details>
+"""
+
+  return html.replace('\t', ' ').replace("'", '’')
+
+
+if __name__ == '__main__':
+  print('Command line access not supported')
