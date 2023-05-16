c6c208432b98b41ec4ecef0480e443b3921713c3
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2023-05-16 11:00:48 -0400

    What happened to NYSED?

--- nys_institutions.py
+++ nys_institutions.py
@@ -5,9 +5,11 @@ import cssselect
 import psycopg
 import re
 import requests
+import sys
 
 from datetime import date
 from lxml.html import document_fromstring
+from pathlib import Path
 from psycopg.rows import namedtuple_row
 from typing import Dict, Tuple
 
@@ -47,12 +49,38 @@ cuny_institutions['yrk'] = ('33500', 'CUNY YORK COLLEGE')
 # Scrape the NYSED website for institution id numbers and names. Sending a POST request with the
 # name "searches" and value "1" gets a page with a form with all institutions and ids as options
 # in a select element.
-url = 'http://www.nysed.gov/coms/rp090/IRPSL1'
-r = requests.post(url, data={'searches': 1})
-html_document = document_fromstring(r.content)
-option_elements = [option.text_content() for option in html_document.cssselect('option')]
-if len(option_elements) < 100:
-  exit(f'{__file__}: ERROR: got only {len(option_elements)} institutions from {url}.')
+""" Getting empty response. It's like the query string isn't getting sent.
+    Much fiddling, including trying to emulate the headers Firefox uses when making a successful
+    request to the same url. No success. Always getting status 200 but the web page returned says
+    "Please page back and check your input, must select a search option" instead of returning a
+    select element with the colleges as option elements.
+"""
+# 'Host': 'www2.nysed.gov', status_code is 404 if present in headers
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:109.0) Gecko/20100101 Firefox/110.0',
+           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+           'Accept-Language': 'en-US,en;q=0.5',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'Content-Type': 'application/x-www-form-urlencoded',
+           'Content-Length': '10',
+           'Origin': 'https://www2.nysed.gov',
+           'Connection': 'keep-alive',
+           'Referer': 'https://www2.nysed.gov/heds/IRPSL1.html',
+           'Upgrade-Insecure-Requests': '1',
+           'Sec-Fetch-Dest': 'document',
+           'Sec-Fetch-Mode': 'navigate',
+           'Sec-Fetch-Site': 'same-origin',
+           'Sec-Fetch-User': '?1'}
+script_file = Path(__file__).name
+url = 'https://www.nysed.gov/coms/rp090/IRPSL1/'
+response = requests.post(url, data={'Searches': "1"})
+if response.status_code == requests.codes.ok:
+  html_document = document_fromstring(response.content)
+  option_elements = [option.text_content() for option in html_document.cssselect('option')]
+  if len(option_elements) < 400:
+    exit(f'{script_file}: ERROR: received {len(option_elements)} institutions from {url} '
+         f'(expected 400+).')
+else:
+  exit(f'{script_file}: ERROR: {url} returned {response.status_code} status')
 
 with psycopg.connect('dbname=cuny_curriculum') as conn:
   with conn.cursor(row_factory=namedtuple_row) as cursor:
